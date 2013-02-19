9b9f3f5360ff8eda609eb5e4ca229e040a4ccaff
Krishna Chaitanya Gourishetti <chaituonly2u@yahoo.com>
2013-02-19 18:24:17 +0530

    Messages API Initial commit

--- model.impl/src/main/java/com/informatica/ilm/pdmds/model/impl/UserGroupImpl.java
+++ model.impl/src/main/java/com/informatica/ilm/pdmds/model/impl/UserGroupImpl.java
@@ -1,11 +1,38 @@
 package com.informatica.ilm.pdmds.model.impl;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.TableGenerator;
+import javax.persistence.Version;
 
 import com.informatica.ilm.pdmds.model.api.UserGroup;
 
 @Entity
 public class UserGroupImpl implements UserGroup {
+	
+	@Id
+	@TableGenerator(name="UserGroupIdGenerator",allocationSize = 200, initialValue = 1, table="ILM_SEQ_GEN", pkColumnName = "SEQ_NAME", pkColumnValue = "USERGROUP", valueColumnName = "SEQ_VALUE")
+	@GeneratedValue(generator = "UserGroupIdGenerator")
+	@Column(name = "ID", precision = 19, scale = 0, nullable = false)
+	private long id;
+	
+	@Version
+	@Column(name = "VERSION", nullable = false)
+	private int version;
+	
+	@Column(name = "NAME", nullable = false, length = 250)
+    private String name;
+
+    @Column(name = "DESCRIPTION", length = 4000)
+    private String description;
+
+    @Column(name = "NAMESPACE", length = 250)
+    private String namespace;
+
+    @Column(name = "STATUS", length = 250)
+    private String status;
 
 	@Override
 	public long getId() {
--- model/src/main/java/com/informatica/ilm/pdmds/model/api/User.java
+++ model/src/main/java/com/informatica/ilm/pdmds/model/api/User.java
@@ -1,9 +1,50 @@
 package com.informatica.ilm.pdmds.model.api;
 
-public interface User extends ILMObject {
+ public interface User extends ILMObject {
 
 	String getName();
 	
 	void setName(String name);
 	
-}
+	 int getVersion();
+
+	 void setVersion(int version); 
+
+	 String getDescription(); 
+
+	 void setDescription(String description); 
+
+	 String getDomainname(); 
+
+	 void setDomainname(String domainname); 
+
+	 String getFullname(); 
+
+	 void setFullname(String fullname); 
+
+	 String getEmail(); 
+
+	 void setEmail(String email); 
+
+	 String getPhone(); 
+
+	 void setPhone(String phone); 
+
+	 boolean isAdmin(); 
+
+	 void setAdmin(boolean admin); 
+
+	 String getNamespace(); 
+
+	 void setNamespace(String namespace); 
+
+	 UserStatus getStatus(); 
+
+	 void setStatus(UserStatus status); 
+
+	 String getPassword(); 
+
+	 void setPassword(String password); 
+
+ }
+
