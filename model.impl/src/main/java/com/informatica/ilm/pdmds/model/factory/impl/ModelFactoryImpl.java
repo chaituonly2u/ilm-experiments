55e86096235713f917e6a10f6aca2c3a76514aec
Krishna Chaitanya Gourishetti <chaituonly2u@yahoo.com>
2013-03-27 17:09:06 +0530

    Model Impl with model factory impl

---
+++ model.impl/src/main/java/com/informatica/ilm/pdmds/model/factory/impl/ModelFactoryImpl.java
@@ -0,0 +1,16 @@
+package com.informatica.ilm.pdmds.model.factory.impl;
+
+import com.informatica.ilm.pdmds.model.api.ILMObject;
+import com.informatica.ilm.pdmds.model.factory.api.ModelFactory;
+
+public class ModelFactoryImpl implements ModelFactory {
+
+	@Override
+	public <T extends ILMObject> T createObject(T clazz)
+			throws InstantiationException, IllegalAccessException,
+			ClassNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
