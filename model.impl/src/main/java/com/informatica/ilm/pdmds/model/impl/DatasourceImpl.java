8272a8b25781be972f2561f5c99e457c8968429d
Krishna Chaitanya Gourishetti <chaituonly2u@yahoo.com>
2013-04-15 17:01:27 +0530

    New pom configuration

--- model.impl/src/main/java/com/informatica/ilm/pdmds/model/impl/DatasourceImpl.java
+++ model.impl/src/main/java/com/informatica/ilm/pdmds/model/impl/DatasourceImpl.java
@@ -22,142 +22,142 @@ import com.informatica.ilm.pdmds.model.api.User;
 @Entity
 public class DatasourceImpl implements Datasource {
 
-	@Id
-	@TableGenerator(name = "DatasourceIdGenerator", allocationSize = 200, initialValue = 1, table = "ILM_SEQ_GEN", pkColumnName = "SEQ_NAME", pkColumnValue = "DATASOURCE", valueColumnName = "SEQ_VALUE")
-	@GeneratedValue(generator = "DatasourceIdGenerator")
-	@Column(name = "ID", precision = 19, scale = 0, nullable = false)
-	private long id;
+    @Id
+    @TableGenerator(name = "DatasourceIdGenerator", allocationSize = 200, initialValue = 1, table = "ILM_SEQ_GEN", pkColumnName = "SEQ_NAME", pkColumnValue = "DATASOURCE", valueColumnName = "SEQ_VALUE")
+    @GeneratedValue(generator = "DatasourceIdGenerator")
+    @Column(name = "ID", precision = 19, scale = 0, nullable = false)
+    private long id;
 
-	@Version
-	@Column(name = "VERSION", nullable = false)
-	private int version;
+    @Version
+    @Column(name = "VERSION", nullable = false)
+    private int version;
 
-	@Column(name = "NAME", nullable = false, length = 250)
-	private String name;
+    @Column(name = "NAME", nullable = false, length = 250)
+    private String name;
 
-	@Column(name = "DESCRIPTION", length = 4000)
-	private String description;
+    @Column(name = "DESCRIPTION", length = 4000)
+    private String description;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = true, cascade = { CascadeType.REFRESH }, targetEntity = UserImpl.class)
-	@JoinColumn(name = "ID_CREATED_BY")
-	private User createdBy;
+    @ManyToOne(fetch = FetchType.LAZY, optional = true, cascade = {CascadeType.REFRESH}, targetEntity = UserImpl.class)
+    @JoinColumn(name = "ID_CREATED_BY")
+    private User createdBy;
 
-	@Column(name = "CREATION_TIME", precision = 19, scale = 0, nullable = false)
-	private long createdAt;
+    @Column(name = "CREATION_TIME", precision = 19, scale = 0, nullable = false)
+    private long createdAt;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = true, cascade = { CascadeType.REFRESH }, targetEntity = UserImpl.class)
-	@JoinColumn(name = "ID_UPDATED_BY")
-	private User updatedBy;
+    @ManyToOne(fetch = FetchType.LAZY, optional = true, cascade = {CascadeType.REFRESH}, targetEntity = UserImpl.class)
+    @JoinColumn(name = "ID_UPDATED_BY")
+    private User updatedBy;
 
-	@Column(name = "UPDATION_TIME", precision = 19, scale = 0, nullable = false)
-	private long updatedAt;
-	
-	@Column(name = "LAST_IMPORT_TIME", nullable = true, precision = 19, scale = 0)
+    @Column(name = "UPDATION_TIME", precision = 19, scale = 0, nullable = false)
+    private long updatedAt;
+
+    @Column(name = "LAST_IMPORT_TIME", nullable = true, precision = 19, scale = 0)
     private long lastImportAt;
-	
-	@Column(name = "STATUS", nullable = false, length = 250)
+
+    @Column(name = "STATUS", nullable = false, length = 250)
     private String status;
-	
-	@ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = {CascadeType.REFRESH}, targetEntity = ProjectImpl.class)
-	@JoinColumn(name = "ID_PROJECT")
-	private Project project;
 
-	@OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY, mappedBy = "dataSource", orphanRemoval = true, targetEntity = SourceImpl.class)
-	private List<Source> sources;
-	
-	public long getId() {
-		return id;
-	}
+    @ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = {CascadeType.REFRESH}, targetEntity = ProjectImpl.class)
+    @JoinColumn(name = "ID_PROJECT")
+    private Project project;
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY, mappedBy = "dataSource", targetEntity = SourceImpl.class)
+    private List<Source> sources;
 
-	public int getVersion() {
-		return version;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setVersion(int version) {
-		this.version = version;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public int getVersion() {
+        return version;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setVersion(int version) {
+        this.version = version;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public User getCreatedBy() {
-		return createdBy;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setCreatedBy(User createdBy) {
-		this.createdBy = createdBy;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public long getCreatedAt() {
-		return createdAt;
-	}
+    public User getCreatedBy() {
+        return createdBy;
+    }
 
-	public void setCreatedAt(long createdAt) {
-		this.createdAt = createdAt;
-	}
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
 
-	public User getUpdatedBy() {
-		return updatedBy;
-	}
+    public long getCreatedAt() {
+        return createdAt;
+    }
 
-	public void setUpdatedBy(User updatedBy) {
-		this.updatedBy = updatedBy;
-	}
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
 
-	public long getUpdatedAt() {
-		return updatedAt;
-	}
+    public User getUpdatedBy() {
+        return updatedBy;
+    }
 
-	public void setUpdatedAt(long updatedAt) {
-		this.updatedAt = updatedAt;
-	}
+    public void setUpdatedBy(User updatedBy) {
+        this.updatedBy = updatedBy;
+    }
 
-	public long getLastImportAt() {
-		return lastImportAt;
-	}
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
 
-	public void setLastImportAt(long lastImportAt) {
-		this.lastImportAt = lastImportAt;
-	}
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public long getLastImportAt() {
+        return lastImportAt;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public void setLastImportAt(long lastImportAt) {
+        this.lastImportAt = lastImportAt;
+    }
 
-	public Project getProject() {
-		return project;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public void setProject(Project project) {
-		this.project = project;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public List<Source> getSources() {
-		return sources;
-	}
+    public Project getProject() {
+        return project;
+    }
 
-	public void setSources(List<Source> sources) {
-		this.sources = sources;
-	}
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<Source> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<Source> sources) {
+        this.sources = sources;
+    }
 }
